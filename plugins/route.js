60f3a2cd49a66cf6c6832046cda8b5478bb14a73
juan pablo <juanpablolguin@8i.cl>
2022-03-04 02:10:58 -0300

    readme

--- plugins/route.js
+++ plugins/route.js
@@ -1,11 +1,10 @@
 export default function({ app, store }) {
   app.router.afterEach((to, from) => {
-    console.log("algo")
+
   })
 
   app.router.beforeEach((to, from, next) => {
-    console.log("algo 2")
-    // store.commit("TOGGLE_SIDEBAR");
+    // store.commit("");
     next();
   });
 }
